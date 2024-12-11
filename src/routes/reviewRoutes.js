782cf832bd6c04644b26850819b48ee7cfac4694
Ketan Jindal <ketan.jindal@netsmartz.net>
2024-12-11 15:26:24 +0530

    Restruct the DTO

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { addReview } = require('../controllers/reviewController');
-
+const express = require("express");
+const { addReview } = require("../controllers/reviewController");
+const auth = require("../services/auth");
 const router = express.Router();
 
-router.post('/:id/reviews', addReview);
+router.post("/:id/reviews", auth, addReview);
 
 module.exports = router;
--- src/services/mailService.js
+++ src/services/mailService.js
@@ -28,18 +28,11 @@ const sendOtpEmail = async (to, text) => {
 };
 
 const sendPasswordResetEmail = async (email) => {
-  try {
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: "Password Reset",
-      text: "Your password has been successfully reset. If you did not request this change, please contact our support team immediately.",
-    });
-
-    console.log("Password reset email sent:", info.messageId);
-  } catch (error) {
-    console.error("Error sending password reset email:", error);
-  }
+  await sendMail(
+    email,
+    "Password Reset",
+    "Your password has been successfully reset. If you did not request this change, please contact our support team immediately."
+  );
 };
 
 module.exports = {
