2463497e2b45552d8999bd7487739eb54444882a
jindal12ketan <jindal.2002ketan@gmail.com>
2024-12-11 22:38:02 +0530

    fix the error code

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -5,11 +5,11 @@ const loginUser = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "User not found" });
+      return res.status(404).json({ message: "User not found" });
     }
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid password" });
+      return res.status(401).json({ message: "Invalid password" });
     }
     const token = user.generateToken();
     const loginDTO = new LoginDTO(user.name, user.email, token);
